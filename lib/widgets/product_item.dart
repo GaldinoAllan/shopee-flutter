bbe03a74cd606d2049597870b07840732a5c666f
GaldinoAllan <galdinoallancodes@gmail.com>
2022-03-22 18:58:21 -0300

    snack bar feedback when added a product

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -51,6 +51,17 @@ class ProductItem extends StatelessWidget {
                     product.price,
                     product.title,
                   );
+                  ScaffoldMessenger.of(context).hideCurrentSnackBar();
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                    content: const Text('Add item to cart!'),
+                    duration: const Duration(seconds: 3),
+                    action: SnackBarAction(
+                      label: 'UNDO',
+                      onPressed: () {
+                        cart.removeItem(product.id);
+                      },
+                    ),
+                  ));
                 },
                 color: Theme.of(context).colorScheme.secondary,
               ),
